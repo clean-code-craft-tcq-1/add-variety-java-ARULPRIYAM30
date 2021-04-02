fe167a8179ddbb28f30ea4ce1a904d06b1cdc799
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-04-02 08:50:38 +0530

    Add files via upload

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -6,10 +6,23 @@ import org.junit.Test;
 
 public class TypewiseAlertTest 
 {
-    @Test
-    public void infersBreachAsPerLimits()
+   @Test
+    public void checkBateeryLimits()
     {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==BreachType.TOO_LOW);
+      assertTrue(TypewiseAlert.inferBreach(42, 20, 30) ==BreachType.TOO_HIGH);
+      assertTrue(TypewiseAlert.inferBreach(27, 20, 30) ==BreachType.NORMAL);
+    }
+	
+	@Test
+    public void checkCoolingTypeAndAlert()
+    {
+        BatteryCharacter batteryCharacter = new BatteryCharacter(CoolingType.PASSIVE_COOLING,"Power Shell Battery");
+		TypewiseAlert.checkAndAlert(AlertTarget.TO_Email,batteryCharacter , 36);
+    }
+	@Test
+    public void categorizeTemperatureBreachBasedCooling()
+    {
+        assertTrue((TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING,45) == BreachType.TOO_HIGH));
     }
 }
