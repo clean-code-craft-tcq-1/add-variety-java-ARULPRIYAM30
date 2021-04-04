669b47d009fe5a75269abfdb52ff60c6c4c5085e
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-04-04 16:32:06 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,28 +1,33 @@
-package TypewiseAlert;
+package Test;
 
-import static org.junit.Assert.assertTrue;
-
+import java.lang.reflect.InvocationTargetException;
+import static org.junit.Assert.*;
 import org.junit.Test;
+import TypewiseAlert.*;
+import TypewiseAlert.BreachType;
 
-public class TypewiseAlertTest 
-{
-   @Test
-    public void checkBateeryLimits()
-    {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==BreachType.TOO_LOW);
-      assertTrue(TypewiseAlert.inferBreach(42, 20, 30) ==BreachType.TOO_HIGH);
-      assertTrue(TypewiseAlert.inferBreach(27, 20, 30) ==BreachType.NORMAL);
-    }
-	
+public class TypewiseAlertTest {
 	@Test
-    public void checkCoolingTypeAndAlert()
+    public void infersBreachAsPerLimits()
     {
-        BatteryCharacter batteryCharacter = new BatteryCharacter(CoolingType.PASSIVE_COOLING,"Power Shell Battery");
-		TypewiseAlert.checkAndAlert(AlertTarget.TO_Email,batteryCharacter , 36);
-    }
+		assertTrue(TypewiseAlert.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
+		assertTrue(TypewiseAlert.inferBreach(42, 20, 30) == BreachType.TOO_HIGH);
+		assertTrue(TypewiseAlert.inferBreach(27, 20, 30) == BreachType.NORMAL);
+	}	
+	 @Test
+    public void infersTemperatureBreach()
+    {
+		assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 50) == BreachType.TOO_HIGH);
+	}
 	@Test
-    public void categorizeTemperatureBreachBasedCooling()
+    public void infersBreachAlertToController()
     {
-        assertTrue((TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING,45) == BreachType.TOO_HIGH));
-    }
+		BatteryInfo batteryproperties = new BatteryInfo(CoolingType.HI_ACTIVE_COOLING, "PowerShell battery");
+		TypewiseAlert.checkAndAlert(AlertType.TO_CONTROLLER, batteryproperties, 60);
+	}
+	public void infersBreachAlertToEmail()
+    {
+		BatteryInfo batteryproperties = new BatteryInfo(CoolingType.PASSIVE_COOLING, "Grid PowerCell");
+		TypewiseAlert.checkAndAlert(AlertType.TO_EMAIL, batteryproperties, 50);
+	}
 }
