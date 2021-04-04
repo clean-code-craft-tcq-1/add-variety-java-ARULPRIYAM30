3e0f4a5932c9ba6c2432a416a47f10871ec477d3
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-04-04 16:37:28 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,33 +1,33 @@
-package Test;
+package TypewiseAlert;
 
 import java.lang.reflect.InvocationTargetException;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import TypewiseAlert.*;
 import TypewiseAlert.BreachType;
 
 public class TypewiseAlertTest {
-	@Test
+    @Test
     public void infersBreachAsPerLimits()
     {
 		assertTrue(TypewiseAlert.inferBreach(12, 20, 30) == BreachType.TOO_LOW);
 		assertTrue(TypewiseAlert.inferBreach(42, 20, 30) == BreachType.TOO_HIGH);
 		assertTrue(TypewiseAlert.inferBreach(27, 20, 30) == BreachType.NORMAL);
-	}	
-	 @Test
+    }	
+    @Test
     public void infersTemperatureBreach()
     {
 		assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 50) == BreachType.TOO_HIGH);
-	}
-	@Test
+    }
+    @Test
     public void infersBreachAlertToController()
     {
 		BatteryInfo batteryproperties = new BatteryInfo(CoolingType.HI_ACTIVE_COOLING, "PowerShell battery");
 		TypewiseAlert.checkAndAlert(AlertType.TO_CONTROLLER, batteryproperties, 60);
-	}
-	public void infersBreachAlertToEmail()
+    }
+    public void infersBreachAlertToEmail()
     {
 		BatteryInfo batteryproperties = new BatteryInfo(CoolingType.PASSIVE_COOLING, "Grid PowerCell");
 		TypewiseAlert.checkAndAlert(AlertType.TO_EMAIL, batteryproperties, 50);
-	}
+    }
 }
